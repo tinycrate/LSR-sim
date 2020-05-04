bc68760779e1d0d2ca9dea08e15a9877bf268f73
Ronald Chan <ronaldchanchl0@gmail.com>
2020-05-04 23:21:45 +0800

    Initial Commit

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+/**
+ * The main entry of LSR-sim
+ */
+public class Application {
+
+    /**
+     * The entry point of the application
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        return;
+    }
+}
---
+++ src/MainView.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainView extends JFrame {
+
+    private static final int BOARD_WIDTH_PX = 463;
+    private static final int BOARD_HEIGHT_PX = 598;
+
+    public MainView() {
+        super();
+    }
+
+    public void init() {
+
+    }
+
+    private void addComponents() {
+
+    }
+}
